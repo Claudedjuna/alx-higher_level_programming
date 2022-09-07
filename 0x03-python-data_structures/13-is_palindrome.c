bb00094adfe3c2d5333fdadb37e22b8e9b799db7
Claudedjuna <Abuleclaude@gmail.com>
2022-09-07 02:44:53 -0700

    change the mode of exec

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -42,4 +42,31 @@ int is_palindrome(listint_t **head)
 
 	tmp = *head;
 	while (tmp)
+	{
+		size++;
+		tmp = tmp->next;
+	}
 
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
+
+	return (1);
+}
